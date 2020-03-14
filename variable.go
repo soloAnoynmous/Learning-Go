d80a6387f2deaae3226a4338cfa195612a98aec3
soloAnoynmous <deepdarksolo@gmail.com>
2020-03-14 13:21:23 +0530

    Commit_3 : Using variables in go

---
+++ variable.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	/*
+		Variables can be declared in three ways.
+	*/
+
+	// 1.
+	var i int
+	i = 43
+	fmt.Println(i)
+
+	// 2.
+	var f float32 = 3.14
+	fmt.Println(f)
+
+	// 3.
+	firstName := "satyam"
+	fmt.Println(firstName)
+}
